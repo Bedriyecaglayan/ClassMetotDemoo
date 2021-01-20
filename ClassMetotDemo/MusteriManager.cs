2f6c9bb32b50c3273d5af0511aba4d763bbaffb1
Lenovo-pc <bedis730@gmail.com>
2021-01-20 18:58:21 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteriler)
+        {
+            Console.WriteLine("Kayıtlı Başarılı" + "Hoşgeldiniz Sayın" + musteriler.MusteriAdi + musteriler.MusteriSoyadi);
+            Console.WriteLine(musteriler.Yas + musteriler.Cinsiyet);
+        }
+        public void Delete(Musteri musteriler)
+        {
+            Console.WriteLine("Kayıt silme işlemi gerçekleştirilmiştir." + musteriler.MusteriAdi + musteriler.MusteriSoyadi);
+        }
+        public void List(Musteri musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("müşteri listesi başarıyla görüntülendi.");
+                Console.WriteLine(musteriler.MusteriAdi + " " + musteriler.MusteriSoyadi + " " + musteriler.Yas + " " + musteriler.Cinsiyet);
+            }
+        }
+    }
+}
